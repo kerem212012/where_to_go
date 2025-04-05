bd4d4335cfcb3021395557841aa357866f8fe0ec
kerem212012 <rockstarfoxykerem@gmail.com>
2025-04-05 12:33:41 +0300

    data takeing from db

--- places/views.py
+++ places/views.py
@@ -1,6 +1,30 @@
+from audioop import reverse
+
 from django.shortcuts import render
 
+from places.models import Place
 
 def start_page(request):
-  return render(request, 'index.html', {})
-
+    places = Place.objects.all()
+    geo = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for place in places:
+        geo["features"].append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lng, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": "" #reverse('place_details', kwargs={'place_id': place.id})
+            }
+        }
+        )
+    context = {
+        "geo_json": geo,
+    }
+    return render(request, 'index.html', context=context)
