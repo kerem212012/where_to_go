d575e7c14b72f6414f9ffb822623cbad5a7ad4ed
kerem212012 <rockstarfoxykerem@gmail.com>
2025-04-10 19:46:41 +0300

    add show_preview

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from places.models import Place, Image
 
-
+IMAGE_HEIGHT = 200
+IMAGE_WIDTH = 200
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ("show_preview",)
 
+    def show_preview(self,image):
+        return mark_safe(f"<img src='{image.image.url}' width='{IMAGE_WIDTH}' height= '{IMAGE_HEIGHT}'>")
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
