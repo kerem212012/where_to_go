1f0e94e3d3c72a1850772b9eed9c613414592b03
kerem212012 <rockstarfoxykerem@gmail.com>
2025-04-12 17:09:37 +0300

    fix README and set default arg to settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY',"replace_me")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG',True)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS',["*",])
 
 
 # Application definition
